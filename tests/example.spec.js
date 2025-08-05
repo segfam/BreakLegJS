3cfb7f71dd2913ab1e745a59737d435aa9729b30
Tomer Segal <tomer.segal@sensi.ai>
2025-08-05 11:15:09 +0300

    adding demoQA

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,9 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('demoQA site', async ({page}) => {
+  await page.goto('https://demoqa.com/');
+
+  await expect(page).toHaveTitle(/DEMOQA/);
+});
