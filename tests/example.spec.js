a57e1a6565afd9d48c3a99d071df1c462cc9b55b
Tomer Segal <tomer.segal@sensi.ai>
2025-08-06 14:44:08 +0300

    deleting default tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,23 +1,6 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
 test('verify demoQA site', async ({page}) => {
   await page.goto('https://demoqa.com/');
   await expect(page).toHaveTitle(/DEMOQA/);
